c015a991569893932b88cf3f3fea925a26d589cf
Borys Supreme <bvs.bromes@gmail.com>
2022-12-26 00:35:14 +0200

    'update'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import StyledButton from './Button.styled';
+import Controls from './Controls.styled';
+import StyledTitle from './Title.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handelFeedback(type) {
+    this.setState({ [type]: this.state[type] + 1 });
+  }
+
+  render() {
+    return (
+      <section>
+        <StyledTitle>Please leave feedback</StyledTitle>
+        <Controls className="Feedback__controls">
+          <StyledButton
+            type="button"
+            onClick={() => this.handelFeedback('good')}
+          >
+            Good
+          </StyledButton>
+          <StyledButton
+            type="button"
+            onClick={() => this.handelFeedback('neutral')}
+          >
+            Neutral
+          </StyledButton>
+          <StyledButton
+            type="button"
+            onClick={() => this.handelFeedback('bad')}
+          >
+            Bad
+          </StyledButton>
+        </Controls>
+        <StyledTitle>Statistics</StyledTitle>
+        <p className="Feedback__item">God : {this.state.good}</p>
+        <p className="Feedback__item">Neutral : {this.state.neutral}</p>
+        <p className="Feedback__item">Bad : {this.state.bad}</p>
+      </section>
+    );
+  }
+}
